d9d032b9c02bcc1b33182b3bc79df095072bc77a
DenRassk <112314783+DenRassk@users.noreply.github.com>
2023-10-03 20:35:27 +0500

    Delete 4444 directory

--- 4444/dz1/dz1app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('', views.index, name='Начальная страница сайта'),
-    path('about/', views.about, name='Обо мне'),
-]
